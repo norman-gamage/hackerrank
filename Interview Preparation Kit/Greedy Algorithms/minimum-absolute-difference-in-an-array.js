554c6cdfa4a0a24a2806ac854a7c743cd8170ae3
Norman Gamage <norman.gamage@gmail.com>
2020-05-25 10:51:27 +0800

    Minimum Absolute Difference in an Array

---
+++ Interview Preparation Kit/Greedy Algorithms/minimum-absolute-difference-in-an-array.js
@@ -0,0 +1,18 @@
+/**
+ * Title          Minimum Absolute Difference in an Array
+ * URL            https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array
+ *
+ * Author         Norman Gamage <norman.gamage@gmail.com>
+ * Version        1.0
+ * Last Update    2020 May 25
+ */
+
+function minimumAbsoluteDifference(arr) {
+  arr = arr.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    arr[i] = Math.abs(arr[i] - arr[i + 1]);
+  }
+
+  return Math.min(...arr);
+}
